d51702223397a53c641f17b7548be48566d04f0a
jocker_29 <229482191@qq.com>
2020-03-24 21:12:40 +0800

    no message

--- root/server/game_jpm/room/handler_room.go
+++ root/server/game_jpm/room/handler_room.go
@@ -103,7 +103,7 @@ func (self *Room) JPMMSG_CS_START_JPM_REQ(actor int32, msg []byte, session int64
 		val := reward + (sumOdds * int64(BetNum) / 9)
 		acc.AddMoney(val, common.EOperateType_JPM_WIN)
 		if acc.GetOSType() == 4 {
-			asyn_addMoney(self.addr_url, acc.UnDevice, val, int32(self.roomId), "金品梅 中奖", nil, nil) //中奖
+			asyn_addMoney(5, self.addr_url, acc.UnDevice, val, int32(self.roomId), "金品梅 中奖", nil, nil) //中奖
 		}
 
 		log.Debugf("玩家:%v 结果->>>>>>> 身上的金币:%v 所有中奖线:%+v 一维数组:%v 获得免费次数:%v 总赔率:%v 获得奖金：%v",
@@ -177,7 +177,7 @@ func (self *Room) JPMMSG_CS_START_JPM_REQ(actor int32, msg []byte, session int64
 				}
 				send_tools.Send2Account(protomsg.JPMMSG_SC_START_JPM_RES.UInt16(), resultMsg, session)
 			}
-			asyn_addMoney(self.addr_url, acc.UnDevice, -int64(BetNum), int32(self.roomId), fmt.Sprintf("金瓶梅请求下注:%v", BetNum), back, errback)
+			asyn_addMoney(5, self.addr_url, acc.UnDevice, -int64(BetNum), int32(self.roomId), fmt.Sprintf("金瓶梅请求下注:%v", BetNum), back, errback)
 		} else {
 			back("", int64(acc.GetMoney()-BetNum), 0)
 		}
